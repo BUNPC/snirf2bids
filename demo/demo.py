37a2732643a5584c5ef357e9d683f29546aa051c
Christian Arthur <christfilipus@gmail.com>
2022-04-29 12:55:02 -0400

    Update 04/29 v1 - Moved README.md to the docs folder (to use lazydocs' documentation model) - Updated demo.py to reflect recent changes in snirf2bids.py

--- demo/demo.py
+++ demo/demo.py
@@ -1,20 +1,22 @@
-# A demo to use the fnirs-bids converter
+# A demo to use the fNIRS-BIDS converter
 import snirf2bids
-import pysnirf2
+import os
+
 
 def convert():
-    """ To demonstrate 2 methods of using the fnirs-bids converter
-
+    """
+    To demonstrate 3 methods of using the fNIRS-BIDS converter
     """
 
     #####################
     # Variable initialization
 
-    # a snirf file (input) path
-    snirf_file_path = '[path to folder]/Demo/sub-02_task-test_nirs.snirf'
+    # a SNIRF file (input) path
+    cwd = os.getcwd()
+    snirf_file_path = cwd + 'sub-02_task-test_nirs.snirf'
 
-    # a bids (output) destination directory
-    bids_path = '[path to folder]/Demo'
+    # a BIDS (output) destination directory
+    bids_path = cwd
 
     # a dictionary that holds the participant (subject) information
     subj1 = {"participant_id": 'sub-01',
@@ -22,9 +24,9 @@ def convert():
              "sex": 'M'}
 
     #####################
-    # The easiest way to convert the snirf file is to use the "snirf_to_bids" method,
-    # which requires an input snirf file and output directory.
-    # This function will write a bids subject folder.
+    # The easiest way to convert the SNIRF file is to use the "snirf_to_bids" method,
+    # which requires an input SNIRF file and output directory.
+    # This function will write a BIDS subject folder.
     # The participant information is optional, and the default is None.
     # When the participant information is present, the function will also create a participant.tsv file
 
@@ -33,7 +35,7 @@ def convert():
                              participants=subj1)
 
     #####################
-    # The user can also create a subject object in the memory by using the "Subject" class.
+    # The user can also create a SNIRF 'subject' object in the memory by using the "Subject" class.
 
     # The user can create the subject object by passing a snirf file into the instance.
     # The constructor will pull required subject, session, task, run and participant-related
@@ -42,11 +44,12 @@ def convert():
 
     # There are two ways to output this instance to local file!
     # First one is to output the instance to a series of string in form of a json-like format
-    subject1.export(outputFormat='Text',
-                    fpath=bids_path)
+    textStreamOutput = subject1.json_export()
+    # with open('TextStreamOutput.txt', 'w') as f:
+    #     f.write(textStreamOutput)
 
     # Second choice is to output locally as a subject folder, same as calling "snirf_to_bids"
-    subject1.export(outputFormat='Folder',
-                    fpath=bids_path)
+    subject1.directory_export(fpath=bids_path)
+
 
 convert()
